330a4ab2d6130601d402613c40c5f484dc362a39
skyju <77267141+skyju@users.noreply.github.com>
2021-05-12 18:08:13 +0900

    Add files via upload

---
+++ Java2ws1(제공파일)/src/ws1/java2/entity/Airplane.java
@@ -0,0 +1,26 @@
+package ws1.java2.entity;
+
+public class Airplane extends Vehicle {
+	private int numOfEngine;
+	public void setNumOfEngine(int numOfEngine) {
+		this.numOfEngine = numOfEngine;
+	}
+	public int getNumOfEngine() {
+		return this.numOfEngine;
+	}
+	public Airplane(String modelName,
+			 int maxSpeed,
+			 int numberLimit,
+			 int numOfEngine,
+			 boolean available) {
+		super(modelName, maxSpeed, numberLimit, numOfEngine, available);
+	}
+	public void displayInfo() {
+		if(getAvailable()==true) {
+		super.displayInfo();
+		System.out.println(" 엔진개수 : " + getNumOfEngine() + "개");
+		} else {
+			System.out.println("현재 사용 가능한 상태가 아닙니다.");
+		}
+	}
+}
---
+++ Java2ws1(제공파일)/src/ws1/java2/entity/Vehicle.java
@@ -0,0 +1,48 @@
+package ws1.java2.entity;
+
+public class Vehicle {
+	protected String modelName;
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+	public String getModelName() {
+		return this.modelName;
+	}
+	protected int maxSpeed;
+	public void setMaxSpeed(int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+	public int getMaxSpeed() {
+		return this.maxSpeed;
+	}
+	protected int numberLimit;
+	public void setNumberLimit(int numberLimit) {
+		this.numberLimit = numberLimit;
+	}
+	public int getNumberLimit() {
+		return this.numberLimit;
+	}
+	protected boolean available;
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+	public boolean getAvailable() {
+		return this.available;
+	}
+	public Vehicle(String modelName,
+			 int maxSpeed,
+			 int numberLimit,
+			 int extraInfoNum,
+			 boolean available) {
+		this.modelName = modelName;
+		this.maxSpeed = maxSpeed;
+		this.numberLimit = numberLimit;
+		this.available = available;
+	}
+	public void displayInfo() {
+		System.out.println("-------------------------------------------");
+		System.out.println(" 모 델 명 : " + getModelName());
+		System.out.println(" 최고속도 : " + getMaxSpeed() + "km/h");
+		System.out.println(" 최대정원 : " + getNumberLimit() + "명");
+	}
+}
