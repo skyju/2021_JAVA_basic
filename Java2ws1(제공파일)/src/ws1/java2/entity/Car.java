6d73bf1fbe9686aa0ef69e5f1dc4e6b62862f3ba
skyju <77267141+skyju@users.noreply.github.com>
2021-05-12 18:06:21 +0900

    Add files via upload

---
+++ Java2ws1(제공파일)/src/ws1/java2/entity/Car.java
@@ -0,0 +1,26 @@
+package ws1.java2.entity;
+
+public class Car extends Vehicle {
+	private int mileage;
+	public void setMileage(int mileage) {
+		this.mileage = mileage;
+	}
+	public int getMileage() {
+		return this.mileage;
+	}
+	public Car(String modelName,
+			 int maxSpeed,
+			 int numberLimit,
+			 int mileage,
+			 boolean available) {
+		super(modelName, maxSpeed, numberLimit, mileage, available);
+	}
+	public void displayInfo() {
+		if(getAvailable()==true) {
+		super.displayInfo();
+		System.out.println(" 평균연비 : " + getMileage() + "km/l");
+		} else {
+			System.out.println("현재 사용 가능한 상태가 아닙니다.");
+		}
+	}
+}
