c0e2e2dfb19276117582eb59d8094e673c10626a
skyju <77267141+skyju@users.noreply.github.com>
2021-05-14 17:25:08 +0900

    Add files via upload

--- Workshop03/src/com/test/workshop3/account/Account.java
+++ Workshop03/src/com/test/workshop3/account/Account.java
@@ -4,58 +4,43 @@ import com.test.workshop3.exception.InvalidValueException;
 public class Account {
     
     private String account;
-    public String getAccount() {
-		return account;
-	}
-	public void setAccount(String account) {
-		this.account = account;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getBalance() {
-		return balance;
-	}
-	public void setBalance(int balance) {
-		this.balance = balance;
-	}
 	private String name;
     private int    balance;
-    
-    public Account() {
-    }
+    public Account() {}
     public Account(String account, String name, int balance) throws InvalidValueException {
-        //TODO 최초 입금 금액이 0원 이하인 경우, 예외 발생
     	this.account = account;
         this.name    = name;
         this.balance = balance;
-    	if (balance <= 0) {
+    	if (balance <= 0) {//최초 입금 금액이 0원 이하인 경우 예외 발생
     		throw new InvalidValueException("[에러] 계좌 생성 시, 최초 입금 금액이 필요합니다.");
     	}
+    	if (this.account.equals("")) {
+    		throw new InvalidValueException("[에러] 계좌 생성 시, 계좌 번호 입력이 필요합니다.");
+    	}
+    	if (this.name.equals("")) {
+    		throw new InvalidValueException("[에러] 계좌 생성 시, 예금주 명이 필요합니다.");
+    	}
     }
+    
     public void deposit(int money) throws InvalidValueException {
-        //TODO 입금할 금액이 잘못 입력된 경우, 예외 발생
-        if (money <= 0) {
+        if (money <= 0) {//입금할 금액이 0원 이하인 경우 예외 발생
         	throw new InvalidValueException("[에러] 입금할 금액은 0원 초과의 숫자로 입력하세요.");
         }
         balance += money;
     }
+    
     public void withdraw(int money) throws InvalidValueException {
-        //TODO 출금할 금액이 잘못 입력되었거나, 잔액이 부족한 경우 예외 발생
-        if (money <= 0) {
+        if (money <= 0) {//출금할 금액이 0원 이하인 경우 예외 발생
         	throw new InvalidValueException("[에러] 출금할 금액은 0원 초과의 숫자로 입력하세요.");
         }
-        if (this.balance < money) {
+        if (this.balance < money) {//잔액이 부족한 경우 예외 발생
         	throw new InvalidValueException("[에러] 잔액이부족합니다.");
         }
         balance -= money;
     }
+    
     @Override
     public String toString() {
     	return "[계좌] " + account + "\t[예금주] " + name + "\t[잔액] " + balance;
-
     }
 }
--- Workshop03/src/com/test/workshop3/account/AccountTest.java
+++ Workshop03/src/com/test/workshop3/account/AccountTest.java
@@ -3,28 +3,29 @@ package com.test.workshop3.account;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
 import com.test.workshop3.exception.AccountNotFoundException;
 import com.test.workshop3.exception.InvalidValueException;
 
 public class AccountTest {    
     public static void main( String[] args ) {        
         int menu  = 0;
-        Account acc = new Account();
+        Account acc = null;
         while ( menu != 9 ) {            
             printMenu();            
-            try {                
-                menu = Integer.parseInt(getUserInput());                
-                //TODO 각 메뉴 선택 시 해야 할 일 구현
+            try {
+            	String account;
+            	String name;
+            	int balance;
+                menu = Integer.parseInt(getUserInput());           
                 switch ( menu ) {
                 case 1:     // 계좌 생성
                     System.out.print( "\n계좌번호 : " );
-                    String input1 = getUserInput();
+                    account = getUserInput();
                     System.out.print( "예금주 : " );
-                    String input2 = getUserInput();
+                    name = getUserInput();
                     System.out.print( "최초 입금 : " );
-                    int input3 = Integer.parseInt(getUserInput());
-                    acc = new Account(input1, input2, input3);
+                    balance = Integer.parseInt(getUserInput());
+                    acc = new Account(account, name, balance);
                     break;
                 case 2:     // 계좌 정보 출력
                 	checkAccount(acc);
@@ -49,19 +50,20 @@ public class AccountTest {
                     System.out.println( "[에러] 메뉴를 잘못 입력하였습니다." );
                     break;
                 }
-                //TODO catch 세분화 (각각의 예외 상황 별로)
             } catch (InvalidValueException e) {
             	System.out.println(e.getMessage());
             } catch (AccountNotFoundException e) {
             	System.out.println(e.getMessage());
+            } catch (NumberFormatException e) {
+            	System.out.println("[에러] 메뉴는 숫자로 입력해야 합니다.");
             } catch (Exception e) {
-            	
+            	System.out.println(e.getStackTrace());
             }
-        }
-    }
+        }//while문 종료
+    }//main method
     
     public static void checkAccount(Account acc) throws AccountNotFoundException {
-    	if(acc.getAccount() == null) {
+    	if(acc == null) {
     		throw new AccountNotFoundException("[에러] 계좌가 없습니다.");
     	}
     }
@@ -75,11 +77,9 @@ public class AccountTest {
         System.out.println( "===================" );
         System.out.print( ">> 메뉴 : " );
     }
-    
     public static String getUserInput() {        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String inputString = null;
-        
         try {
             inputString = br.readLine();
         } catch ( IOException e ) {
